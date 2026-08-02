116d15695a791e9c13a1106de680703c6b9851ea
Manasghosh7681 <manasghosh923@gmail.com>
2026-08-02 15:38:43 +0530

    merge conflict

--- Admin/view_room.php
+++ Admin/view_room.php
@@ -12,16 +12,9 @@ if (isset($_SESSION['email'])) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Room Allocation | Hostel Management System</title>
-<<<<<<< HEAD
         <link href="../Bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../Bootstrap/css/all.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-=======
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet">
->>>>>>> c3cb7a6e5a8cb3738b5ced3f009e00f09adc980d
         <style>
             :root {
                 --primary-color: #3498db;
